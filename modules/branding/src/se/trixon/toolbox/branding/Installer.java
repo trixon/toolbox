10ecda74dc624de8bd861a679299df28d5488677
Patrik Karlsson <patrik@trixon.se>
2014-01-22 11:07:35 +0100

    set application.title

--- modules/branding/src/se/trixon/toolbox/branding/Installer.java
+++ modules/branding/src/se/trixon/toolbox/branding/Installer.java
@@ -1,6 +1,9 @@
 package se.trixon.toolbox.branding;
 
+import java.util.ResourceBundle;
+import javax.swing.JFrame;
 import org.openide.modules.ModuleInstall;
+import org.openide.windows.WindowManager;
 
 /**
  *
@@ -11,5 +14,13 @@ public class Installer extends ModuleInstall {
     @Override
     public void restored() {
         AboutInitializer.init();
+        WindowManager.getDefault().invokeWhenUIReady(new Runnable() {
+            @Override
+            public void run() {
+                ResourceBundle bundle = ResourceBundle.getBundle("se/trixon/toolbox/branding/about");
+                JFrame mainFrame = (JFrame) WindowManager.getDefault().getMainWindow();
+                mainFrame.setTitle(bundle.getString("application.title"));
+            }
+        });
     }
 }
