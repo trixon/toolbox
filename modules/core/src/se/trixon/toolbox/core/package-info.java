63c37e0370fce45bf9adf6257b24500e80ef382e
Patrik Karlsson <patrik@trixon.se>
2014-01-25 17:07:42 +0100

    Primary options panel

---
+++ modules/core/src/se/trixon/toolbox/core/package-info.java
@@ -0,0 +1,9 @@
+@OptionsPanelController.ContainerRegistration(id = "Tools",
+        position = 0,
+        categoryName = "se.trixon.toolbox.core.Bundle#tools",
+        iconBase = "se/trixon/toolbox/core/res/toolbox32.png",
+        keywords = "se.trixon.toolbox.core.Bundle#OptionsCategory_Keywords_Tools",
+        keywordsCategory = "Tools")
+package se.trixon.toolbox.core;
+
+import org.netbeans.spi.options.OptionsPanelController;
