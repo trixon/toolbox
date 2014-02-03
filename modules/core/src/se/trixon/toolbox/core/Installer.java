90d9184c010659ee92783e2ab6286d64ae9428dc
Patrik Karlsson <patrik@trixon.se>
2014-02-03 20:25:28 +0100

    Xlog

--- modules/core/src/se/trixon/toolbox/core/Installer.java
+++ modules/core/src/se/trixon/toolbox/core/Installer.java
@@ -3,6 +3,7 @@ package se.trixon.toolbox.core;
 import org.openide.modules.ModuleInstall;
 import org.openide.windows.TopComponent;
 import org.openide.windows.WindowManager;
+import se.trixon.almond.Xlog;
 
 /**
  *
@@ -12,14 +13,13 @@ public class Installer extends ModuleInstall {
 
     @Override
     public void restored() {
-        WindowManager.getDefault().invokeWhenUIReady(new Runnable() {
-            @Override
-            public void run() {
-                openWindow("output");
-            }
-        });
-
-        Toolbox.outln(Toolbox.LOG_TITLE, "Toolbox loaded and ready...");
+//        WindowManager.getDefault().invokeWhenUIReady(new Runnable() {
+//            @Override
+//            public void run() {
+//                openWindow("output");
+//            }
+//        });
+        Xlog.v(Toolbox.LOG_TAG, "Loaded and ready...");
     }
 
     private void openWindow(String id) {
--- modules/core/src/se/trixon/toolbox/core/Toolbox.java
+++ modules/core/src/se/trixon/toolbox/core/Toolbox.java
@@ -1,7 +1,6 @@
 package se.trixon.toolbox.core;
 
 import org.openide.awt.StatusDisplayer;
-import se.trixon.almond.Monitor;
 
 /**
  *
@@ -9,15 +8,7 @@ import se.trixon.almond.Monitor;
  */
 public class Toolbox {
 
-    public static final String LOG_TITLE = "core";
-
-    public static void errln(String name, String message) {
-        new Monitor(name, false, true).errln(message);
-    }
-
-    public static void outln(String name, String message) {
-        new Monitor(name, false, true).outln(message);
-    }
+    public static final String LOG_TAG = "Toolbox";
 
     public static void setStatusText(String text, int importance) {
         StatusDisplayer.getDefault().setStatusText(text, importance);
