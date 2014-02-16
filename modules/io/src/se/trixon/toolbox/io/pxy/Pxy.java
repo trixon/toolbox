431a62a7d77e0675fca3d9790cf288fecadfdbe9
Patrik Karlsson <patrik@trixon.se>
2014-02-16 18:21:14 +0100

    PXY reader/writer

---
+++ modules/io/src/se/trixon/toolbox/io/pxy/Pxy.java
@@ -0,0 +1,150 @@
+/* 
+ * Copyright 2014 Patrik Karlsson.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package se.trixon.toolbox.io.pxy;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import se.trixon.toolbox.io.Io;
+
+/**
+ *
+ * @author Patrik Karlsson <patrik@trixon.se>
+ */
+public class Pxy {
+
+    public static FileNameExtensionFilter getFileNameExtensionFilter() {
+        return new FileNameExtensionFilter("*.pxy", "pxy");
+    }
+    private final Charset mCharset = Charset.forName("windows-1252");
+    private String mDate = "";
+    private String mDescription = "";
+    private String mIdText = "XYZ-COORD-FILE";
+    private List<PxyPoint> mPoints = new LinkedList<PxyPoint>();
+    private BufferedReader mReader;
+    private String mReserved1 = "";
+    private String mReserved2 = "";
+    private String mVersion = "V1.00";
+    private BufferedWriter mWriter;
+
+    public Pxy() {
+        mDate = new SimpleDateFormat("yyyy-MM-dd").format(new Date(System.currentTimeMillis()));
+    }
+
+    public void addPoint(PxyPoint pxyPoint) {
+        mPoints.add(pxyPoint);
+    }
+
+    public String getDate() {
+        return mDate;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public String getIdText() {
+        return mIdText;
+    }
+
+    public String getReserved1() {
+        return mReserved1;
+    }
+
+    public String getReserved2() {
+        return mReserved2;
+    }
+
+    public String getVersion() {
+        return mVersion;
+    }
+
+    public void read(File file) throws IOException {
+        mReader = Files.newBufferedReader(file.toPath(), mCharset);
+        String line = mReader.readLine();
+        setIdText(line.substring(0, 16));
+        setVersion(line.substring(17, 22));
+        setDate(line.substring(23, 33));
+        setReserved1(line.substring(34, 73));
+
+        line = mReader.readLine();
+        setDescription(line.substring(0, 38));
+        setReserved2(line.substring(41, 73));
+
+        while ((line = mReader.readLine()) != null) {
+            mPoints.add(new PxyPoint(line));
+        }
+
+        mReader.close();
+    }
+
+    public void setDate(String date) {
+        date = Io.stripString(date, 10);
+        if (!date.isEmpty()) {
+            mDate = date;
+        }
+    }
+
+    public void setDescription(String description) {
+        mDescription = Io.stripString(description, 39);
+    }
+
+    public void setIdText(String idText) {
+        mIdText = Io.stripString(idText, 16);
+    }
+
+    public void setReserved1(String reserved1) {
+        mReserved1 = Io.stripString(reserved1, 39);
+    }
+
+    public void setReserved2(String reserved2) {
+        mReserved2 = Io.stripString(reserved2, 33);
+    }
+
+    public void setVersion(String version) {
+        mVersion = Io.stripString(version, 5);
+    }
+
+    public void write(File file) throws IOException {
+        mWriter = Files.newBufferedWriter(file.toPath(), mCharset);
+
+        StringBuilder builder = new StringBuilder();
+        builder.append(String.format("%-16s,", mIdText));
+        builder.append(String.format("%-5s,", mVersion));
+        builder.append(String.format("%-10s,", mDate));
+        builder.append(String.format("%-40s", mReserved1));
+        builder.append(",\r\n");
+        builder.append(String.format("%-40s,", mDescription));
+        builder.append(String.format("%-33s", mReserved2));
+        builder.append(",\r\n");
+
+        mWriter.write(builder.toString());
+
+        for (PxyPoint pxyPoint : mPoints) {
+            mWriter.write(pxyPoint.toString());
+        }
+
+        mWriter.close();
+    }
+}
---
+++ modules/io/src/se/trixon/toolbox/io/pxy/PxyPoint.java
@@ -0,0 +1,129 @@
+/* 
+ * Copyright 2014 Patrik Karlsson.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package se.trixon.toolbox.io.pxy;
+
+import java.util.Locale;
+import se.trixon.toolbox.io.Io;
+
+/**
+ *
+ * @author Patrik Karlsson <patrik@trixon.se>
+ */
+public class PxyPoint {
+
+    private String mPointCode = "";
+    private String mPointId = "";
+    private String mRemark = "";
+    private String mSpecialCode = "";
+    private double mX;
+    private double mY;
+    private double mZ;
+
+    public PxyPoint() {
+    }
+
+    public PxyPoint(String line) throws NumberFormatException {
+        setPointId(line.substring(0, 12));
+        mX = Double.parseDouble(line.substring(14, 26));
+        mY = Double.parseDouble(line.substring(26, 38));
+        mZ = Double.parseDouble(line.substring(38, 50));
+        setPointCode(line.substring(51, 59));
+        setSpecialCode(line.substring(60, 62));
+        setRemark(line.substring(62, 73));
+    }
+
+    public PxyPoint(String pointId, double x, double y, double z, String pointCode) {
+        setPointId(pointId);
+        mX = x;
+        mY = y;
+        mZ = z;
+        setPointCode(pointCode);
+    }
+
+    public String getPointCode() {
+        return mPointCode;
+    }
+
+    public String getPointId() {
+        return mPointId;
+    }
+
+    public String getRemark() {
+        return mRemark;
+    }
+
+    public String getSpecialCode() {
+        return mSpecialCode;
+    }
+
+    public double getX() {
+        return mX;
+    }
+
+    public double getY() {
+        return mY;
+    }
+
+    public double getZ() {
+        return mZ;
+    }
+
+    public final void setPointCode(String pointCode) {
+        mPointCode = Io.stripString(pointCode, 8);
+    }
+
+    public final void setPointId(String pointId) {
+        mPointId = Io.stripString(pointId, 12);
+    }
+
+    public final void setRemark(String remark) {
+        mRemark = Io.stripString(remark, 12);
+    }
+
+    public final void setSpecialCode(String specialCode) {
+        mSpecialCode = Io.stripString(specialCode, 2);
+    }
+
+    public void setX(double x) {
+        mX = x;
+    }
+
+    public void setY(double y) {
+        mY = y;
+    }
+
+    public void setZ(double z) {
+        mZ = z;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append(String.format("%-12s", mPointId));
+        builder.append("  ");
+        builder.append(String.format(Locale.ENGLISH, "%12.4f", mX));
+        builder.append(String.format(Locale.ENGLISH, "%12.4f", mY));
+        builder.append(String.format(Locale.ENGLISH, "%12.4f", mZ));
+        builder.append(" ");
+        builder.append(String.format("%-8s", mPointCode));
+        builder.append(" ");
+        builder.append(String.format("%-2s", mSpecialCode));
+        builder.append(String.format("%-12s", mRemark));
+        builder.append(",\r\n");
+
+        return builder.toString();
+    }
+}
