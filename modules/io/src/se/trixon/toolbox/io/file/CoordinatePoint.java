9597f56d76bf8e6f542fb08bef1e023c23a593cf
Patrik Karlsson <patrik@trixon.se>
2014-03-05 20:10:15 +0100

    [IO] * NEW: Base class for coordinate points.

---
+++ modules/io/src/se/trixon/toolbox/io/file/CoordinatePoint.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2014 Patrik Karlsson.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package se.trixon.toolbox.io.file;
+
+/**
+ *
+ * @author Patrik Karlsson <patrik@trixon.se>
+ */
+public class CoordinatePoint {
+
+    protected double mX;
+    protected double mY;
+    protected double mZ;
+
+    public double getX() {
+        return mX;
+    }
+
+    public double getY() {
+        return mY;
+    }
+
+    public double getZ() {
+        return mZ;
+    }
+
+    public void setX(double x) {
+        mX = x;
+    }
+
+    public void setY(double y) {
+        mY = y;
+    }
+
+    public void setZ(double z) {
+        mZ = z;
+    }
+
+}
