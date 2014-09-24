927edcfb5449d1b135016d68491229905c2a74cf
Patrik Karlsson <patrik@trixon.se>
2014-09-24 20:34:14 +0200

    New protected member

--- modules/core/src/se/trixon/toolbox/core/base/ToolTopComponent.java
+++ modules/core/src/se/trixon/toolbox/core/base/ToolTopComponent.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright 2014 Patrik Karlsson.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,15 +13,17 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package se.trixon.toolbox.core.base;
-
-import org.openide.windows.TopComponent;
-
-/**
- *
- * @author Patrik Karlsson <patrik@trixon.se>
- */
-public abstract class ToolTopComponent extends TopComponent {
-
-    protected String mToolName;
-}
+package se.trixon.toolbox.core.base;
+
+import java.util.ResourceBundle;
+import org.openide.windows.TopComponent;
+
+/**
+ *
+ * @author Patrik Karlsson <patrik@trixon.se>
+ */
+public abstract class ToolTopComponent extends TopComponent {
+
+    protected ResourceBundle mBundle;
+    protected String mToolName;
+}
