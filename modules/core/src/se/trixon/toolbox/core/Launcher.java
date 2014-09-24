8164955610533d01b1b7255ffb77de149a49f534
Patrik Karlsson <patrik@trixon.se>
2014-09-24 20:35:14 +0200

    Launcher for external processes

---
+++ modules/core/src/se/trixon/toolbox/core/Launcher.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright 2014 Patrik Karlsson.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package se.trixon.toolbox.core;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import org.openide.util.Exceptions;
+
+/**
+ *
+ * @author Patrik Karlsson <patrik@trixon.se>
+ */
+public class Launcher {
+
+    private final Set<Launchable> mLaunchables = new HashSet<>();
+    private Process mProcess;
+    private final ProcessBuilder mProcessBuilder = new ProcessBuilder(new String[]{"/home/pata/ticktock.sh"});
+
+    public Launcher() {
+    }
+
+    public Launcher(Launchable launchable) {
+        mLaunchables.add(launchable);
+    }
+
+    public boolean addLaunchable(Launchable launchable) {
+        return mLaunchables.add(launchable);
+    }
+
+    public void clearLaunchables() {
+        mLaunchables.clear();
+    }
+
+    public void destroy() {
+        mProcess.destroy();
+    }
+
+    public Process getProcess() {
+        return mProcess;
+    }
+
+    public ProcessBuilder getProcessBuilder() {
+        return mProcessBuilder;
+    }
+
+    public void launch() throws IOException, InterruptedException {
+        mProcess = mProcessBuilder.start();
+
+        new Thread(new MessageProcessor(mProcess.getInputStream(), Mode.STD)).start();
+        new Thread(new MessageProcessor(mProcess.getErrorStream(), Mode.ERR)).start();
+
+        mProcess.waitFor();
+
+        for (Launchable launchable : mLaunchables) {
+            launchable.launcherFinished(mProcess.exitValue());
+        }
+    }
+
+    public enum Mode {
+
+        ERR, STD;
+    }
+
+    public interface Launchable {
+
+        public void launcherFinished(int exitValue);
+
+        public void launcherMessage(String message, Mode mode);
+    }
+
+    class MessageProcessor implements Runnable {
+
+        private final InputStream mInputStream;
+        private final Mode mMode;
+
+        public MessageProcessor(InputStream inputStream, Mode mode) {
+            mInputStream = inputStream;
+            mMode = mode;
+        }
+
+        @Override
+        public void run() {
+            InputStreamReader inputStreamReader = new InputStreamReader(mInputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            try {
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    for (Launchable launchable : mLaunchables) {
+                        launchable.launcherMessage(line, mMode);
+                    }
+                }
+            } catch (IOException ex) {
+                Exceptions.printStackTrace(ex);
+                for (Launchable launchable : mLaunchables) {
+                    launchable.launcherMessage(ex.getLocalizedMessage(), Mode.ERR);
+                }
+            }
+        }
+    }
+}
