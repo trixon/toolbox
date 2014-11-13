64507c19b0b6fccd8ea2d86d0d164e4a915ca1a9
Patrik Karlsson <patrik@trixon.se>
2014-11-13 20:21:13 +0100

    Toolbar icon size

--- modules/core/src/se/trixon/toolbox/core/base/ToolTopComponent.java
+++ modules/core/src/se/trixon/toolbox/core/base/ToolTopComponent.java
@@ -24,6 +24,7 @@ import org.openide.windows.TopComponent;
  */
 public abstract class ToolTopComponent extends TopComponent {
 
+    protected static final int TOOLBAR_ICON_SIZE = 24;
     protected ResourceBundle mBundle;
     protected String mToolName;
 }
